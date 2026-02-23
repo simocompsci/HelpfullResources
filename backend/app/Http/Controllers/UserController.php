246f91b64f10e524db6a4889d6dc8d072cb6dbeb
simocompsci <hamids9alo7777@gmail.com>
2026-02-23 15:26:55 +0000

    adding controller structure

--- backend/app/Http/Controllers/UserController.php
+++ backend/app/Http/Controllers/UserController.php
@@ -2,9 +2,23 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\Http\Requests\StoreUserRequest;
+use App\Services\UserService;
+use Symfony\Component\HttpFoundation\Request;
 
 class UserController extends Controller
 {
-    //
+    protected UserService $userService;
+
+    public function __construct(UserService $userService)
+    {
+        $this->userService = $userService;
+    }
+
+    public function store(Request $request)
+    {
+        // $user = $this->userService->createUser($request);
+
+        // return response()->json($user, 201);
+    }
 }
