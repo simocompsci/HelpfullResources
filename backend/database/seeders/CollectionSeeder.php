dc3d6565960a518a3f9b84a666349e53b0fac972
simocompsci <hamids9alo7777@gmail.com>
2026-02-27 21:18:31 +0000

    adding user and collection seeders

--- backend/database/seeders/CollectionSeeder.php
+++ backend/database/seeders/CollectionSeeder.php
@@ -2,16 +2,23 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Collection;
+use App\Models\User;
 
 class CollectionSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     */
     public function run(): void
     {
-        //
+        // For each user, create 3 root collections and 2 child collections each
+        User::all()->each(function ($user) {
+            $rootCollections = Collection::factory()->count(3)->create([
+                'user_id' => $user->id
+            ]);
+
+            $rootCollections->each(function ($root) {
+                Collection::factory()->count(2)->childOf($root)->create();
+            });
+        });
     }
 }
