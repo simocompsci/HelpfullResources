1b2d24bcd744dad9cefb77c0ba52c74b8eb76c70
simocompsci <hamids9alo7777@gmail.com>
2026-02-27 21:19:29 +0000

    adding resource seeders

--- backend/database/seeders/ResourceSeeder.php
+++ backend/database/seeders/ResourceSeeder.php
@@ -2,16 +2,28 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Resource;
+use App\Models\Collection;
+use App\Models\User;
 
 class ResourceSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     */
     public function run(): void
     {
-        //
+        User::all()->each(function ($user) {
+            // 5 resources per user without collection
+            Resource::factory()->count(5)->create([
+                'user_id' => $user->id
+            ]);
+
+            // 3 resources per collection
+            Collection::where('user_id', $user->id)->each(function ($collection) use ($user) {
+                Resource::factory()->count(3)->create([
+                    'user_id' => $user->id,
+                    'collection_id' => $collection->id
+                ]);
+            });
+        });
     }
 }
