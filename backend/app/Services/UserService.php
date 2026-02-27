a24251338d87ef27210396b9c6d5e184210d7dc0
simocompsci <hamids9alo7777@gmail.com>
2026-02-27 20:26:10 +0000

    adding the user service

--- backend/app/Services/UserService.php
+++ backend/app/Services/UserService.php
@@ -2,21 +2,68 @@
 
 namespace App\Services;
 
-use App\Repositories\UserRepository;
+use App\Models\User;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
+use Illuminate\Support\Str;
 
-class UserService{
-    protected UserRepository $userRepository;
-
-    public function __construct(UserRepository $userRepository)
+class UserService
+{
+    public function createAdmin(array $data): User
     {
-        $this->userRepository = $userRepository;
+        return User::create([
+            'id' => Str::uuid(),
+            'firstname' => $data['firstname'],
+            'lastname' => $data['lastname'],
+            'username' => $data['username'],
+            'email' => $data['email'],
+            'role' => 'ADMIN',
+            'password' => Hash::make($data['password']),
+        ]);
     }
 
-    public function createUser(array $data)
+    public function createUser(array $data): User
     {
-        $data['password'] = Hash::make($data['password']);
+        return User::create([
+            'id' => Str::uuid(),
+            'firstname' => $data['firstname'],
+            'lastname' => $data['lastname'],
+            'username' => $data['username'],
+            'email' => $data['email'],
+            'role' => 'USER',
+            'password' => Hash::make($data['password']),
+        ]);
+    }
 
-        return $this->userRepository->create($data);
+    public function getUsers()
+    {
+        return User::all();
+    }
+
+    public function getUser(string $id): User
+    {
+        return User::findOrFail($id);
+    }
+
+    public function updateUser(array $data, string $id): User
+    {
+        $user = User::findOrFail($id);
+
+        if ($user->email !== $data['email']) {
+            if (User::where('email', $data['email'])->exists()) {
+                throw new \Exception("Email already in use");
+            }
+        }
+
+        $user->update([
+            'username' => $data['username'],
+        ]);
+
+        return $user;
+    }
+
+    public function deleteUser(string $id): void
+    {
+        User::findOrFail($id)->delete();
     }
 }
