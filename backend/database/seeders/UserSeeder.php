04ec6beffa430ee0d2a2db54be0eec21d9fe0a65
simocompsci <hamids9alo7777@gmail.com>
2026-02-27 21:17:39 +0000

    adding resource factory

--- backend/database/seeders/UserSeeder.php
+++ backend/database/seeders/UserSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -10,8 +11,17 @@ class UserSeeder extends Seeder
     /**
      * Run the database seeds.
      */
+    
     public function run(): void
     {
-        //
+        // 10 random users
+        User::factory()->count(10)->create();
+
+        // Optional: create an admin
+        User::factory()->create([
+            'email' => 'admin@example.com',
+            'role' => 'ADMIN',
+            'password' => bcrypt('admin123'),
+        ]);
     }
 }
