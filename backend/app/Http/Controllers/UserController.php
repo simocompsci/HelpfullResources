c7841edb557dab432608f9574620b304f75cf88a
simocompsci <hamids9alo7777@gmail.com>
2026-02-27 20:42:48 +0000

    adding user controller

--- backend/app/Http/Controllers/UserController.php
+++ backend/app/Http/Controllers/UserController.php
@@ -2,23 +2,55 @@
 
 namespace App\Http\Controllers;
 
-use App\Http\Requests\StoreUserRequest;
 use App\Services\UserService;
-use Symfony\Component\HttpFoundation\Request;
+use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
-    protected UserService $userService;
+    private UserService $userService;
 
     public function __construct(UserService $userService)
     {
         $this->userService = $userService;
     }
 
+    // GET /api/users
+    public function index()
+    {
+        return response()->json(
+            $this->userService->getUsers()
+        );
+    }
+
+    // GET /api/users/{id}
+    public function show(string $id)
+    {
+        return response()->json(
+            $this->userService->getUser($id)
+        );
+    }
+
+    // POST /api/users
     public function store(Request $request)
     {
-        // $user = $this->userService->createUser($request);
+        return response()->json(
+            $this->userService->createUser($request->all()),
+            201
+        );
+    }
 
-        // return response()->json($user, 201);
+    // PUT /api/users/{id}
+    public function update(Request $request, string $id)
+    {
+        return response()->json(
+            $this->userService->updateUser($request->all(), $id)
+        );
+    }
+
+    // DELETE /api/users/{id}
+    public function destroy(string $id)
+    {
+        $this->userService->deleteUser($id);
+        return response()->json(null, 204);
     }
 }
