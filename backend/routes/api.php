847f20b009ea8a08b35a9fd1678b930579f8c45e
simocompsci <hamids9alo7777@gmail.com>
2026-02-27 20:49:23 +0000

    added routing for now , and will checke later

---
+++ backend/routes/api.php
@@ -0,0 +1,17 @@
+<?php
+
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\CollectionController;
+use App\Http\Controllers\ResourceController;
+use Illuminate\Routing\Route;
+
+Route::apiResource('users', UserController::class);
+Route::apiResource('collections', CollectionController::class);
+Route::apiResource('resources', ResourceController::class);
+
+// Custom routes
+Route::get('users/{userId}/collections', [CollectionController::class, 'getByUser']);
+Route::get('users/{userId}/collections/{parentId}', [CollectionController::class, 'getByUserAndParent']);
+
+Route::get('users/{userId}/resources', [ResourceController::class, 'getUserResources']);
+Route::get('users/{userId}/collections/{collectionId}/resources', [ResourceController::class, 'getByUserAndCollection']);
