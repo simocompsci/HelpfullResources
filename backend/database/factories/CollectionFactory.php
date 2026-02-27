d3c0938e33e301906d5b5f29f7dd56cc74f6bbb3
simocompsci <hamids9alo7777@gmail.com>
2026-02-27 20:55:32 +0000

    adding collection factory

--- backend/database/factories/CollectionFactory.php
+++ backend/database/factories/CollectionFactory.php
@@ -2,6 +2,8 @@
 
 namespace Database\Factories;
 
+use App\Models\Collection;
+use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
@@ -14,10 +16,26 @@ class CollectionFactory extends Factory
      *
      * @return array<string, mixed>
      */
+    protected $model = Collection::class;
+
     public function definition(): array
     {
         return [
-            //
+            'id' => $this->faker->uuid(),
+            'user_id' => User::factory(), // automatically create user if not provided
+            'parent_id' => null, // by default root collection
+            'name' => $this->faker->words(2, true),
         ];
+    }
+
+    // Optional: define a child collection
+    public function childOf(Collection $parent)
+    {
+        return $this->state(function () use ($parent) {
+            return [
+                'parent_id' => $parent->id,
+                'user_id' => $parent->user_id,
+            ];
+        });
     }
 }
