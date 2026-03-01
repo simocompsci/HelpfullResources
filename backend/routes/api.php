eda449b8507d4b00346fbba0c4beb229f9b95ad0
simocompsci <hamids9alo7777@gmail.com>
2026-03-01 16:53:36 +0000

    fixing routes prefix

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -3,7 +3,7 @@
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\CollectionController;
 use App\Http\Controllers\ResourceController;
-use Illuminate\Routing\Route;
+use Illuminate\Support\Facades\Route;
 
 Route::prefix('v1')->group(function () {
 
