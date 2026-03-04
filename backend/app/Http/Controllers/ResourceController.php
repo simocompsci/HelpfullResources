bd53cfc7a3ceb71435ff278b6a386c7d3a356f37
simocompsci <hamids9alo7777@gmail.com>
2026-03-04 22:38:36 +0000

    updating controllers to use api resources

--- backend/app/Http/Controllers/ResourceController.php
+++ backend/app/Http/Controllers/ResourceController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Services\ResourceService;
 use App\Http\Requests\CreateResourceRequest;
 use App\Http\Requests\UpdateResourceRequest;
+use App\Http\Resources\ResourceResource;
 use Illuminate\Http\Request;
 
 class ResourceController extends Controller
@@ -18,57 +19,60 @@ class ResourceController extends Controller
 
     public function index(Request $request)
     {
-        return response()->json(
-            $this->resourceService->getResources(
-                $request->get('page', 1),
-                $request->get('per_page', 20)
-            )
+        $resources = $this->resourceService->getResources(
+            $request->get('page', 1),
+            $request->get('per_page', 20)
         );
+
+        return ResourceResource::collection($resources);
     }
 
     public function show(string $id)
     {
-        return response()->json(
+        return new ResourceResource(
             $this->resourceService->getResource($id)
         );
     }
 
     public function store(CreateResourceRequest $request)
     {
-        return response()->json(
-            $this->resourceService->addResource($request->validated()),
-            201
-        );
+        $resource = $this->resourceService
+            ->addResource($request->validated());
+
+        return new ResourceResource($resource);
     }
 
     public function getUserResources(Request $request, string $userId)
     {
-        return response()->json(
-            $this->resourceService->getAllUserResources(
-                $userId,
-                $request->query('title'),
-                $request->query('type')
-            )
+        $resources = $this->resourceService->getAllUserResources(
+            $userId,
+            $request->query('title'),
+            $request->query('type')
         );
+
+        return ResourceResource::collection($resources);
     }
 
     public function getByUserAndCollection(string $userId, string $collectionId)
     {
-        return response()->json(
-            $this->resourceService->getByUserAndCollection($userId, $collectionId)
+        return ResourceResource::collection(
+            $this->resourceService
+                ->getByUserAndCollection($userId, $collectionId)
         );
     }
 
     public function update(UpdateResourceRequest $request, string $id)
     {
-        return response()->json(
-            $this->resourceService->updateResource($request->validated(), $id)
-        );
+        $resource = $this->resourceService
+            ->updateResource($request->validated(), $id);
+
+        return new ResourceResource($resource);
     }
 
     public function destroy(string $id)
     {
         $this->resourceService->deleteById($id);
+
         return response()->json(null, 204);
     }
 }
