aae13417015f84b56fd2fcc17a7b81675379b7f4
simocompsci <hamids9alo7777@gmail.com>
2026-03-09 22:40:38 +0000

    working on auth

--- backend/app/Http/Controllers/AuthController.php
+++ backend/app/Http/Controllers/AuthController.php
@@ -2,15 +2,28 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
+use AuthenticationRequest;
 use Illuminate\Http\Request;
+use RegisterUserRequest;
 
 class AuthController extends Controller
 {
-    public function login(){}
+    public function login(AuthenticationRequest $request){
+        $user = User::where('username' , $request->username)->first();
+    }
 
-    public function register(){}
+    public function register(RegisterUserRequest $request){}
 
     public function logout(){}
 
     public function me(){}
+
+
+    // will probably add these later
+    // logoutAll()	POST /auth/logout-all	revoke all tokens
+    // refreshToken()	POST /auth/refresh	issue new token
+    // changePassword()	POST /auth/change-password	update password
+    // forgotPassword()	POST /auth/forgot-password	send reset email
+    // resetPassword()  POST /auth/reset-password   reset password
 }
