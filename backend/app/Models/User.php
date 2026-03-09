a7a3971018c784b2b4db6e3650888ab90788fcbd
simocompsci <hamids9alo7777@gmail.com>
2026-03-09 22:28:39 +0000

    adding some configs for token based auth

--- backend/app/Models/User.php
+++ backend/app/Models/User.php
@@ -8,10 +8,11 @@ use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable
 {
-    use HasFactory, HasUuids;
+    use HasFactory, HasUuids , HasApiTokens , Notifiable ;
 
     protected $primaryKey = 'id';
     public $incrementing = false;
