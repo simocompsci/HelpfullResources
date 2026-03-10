15d04292e246559e954efc1ac6a6a46556108112
simocompsci <hamids9alo7777@gmail.com>
2026-03-10 23:01:50 +0000

    working on auth

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -6,25 +6,26 @@ use App\Http\Controllers\CollectionController;
 use App\Http\Controllers\ResourceController;
 use Illuminate\Support\Facades\Route;
 
-Route::post('v1/login', [AuthController::class, 'login']);
-Route::post('v1/register', [AuthController::class, 'register']);
+Route::post('login', [AuthController::class, 'login']);
+Route::post('register', [AuthController::class, 'register']);
 
 
-Route::prefix('v1')->group(function () {
+Route::middleware('auth:sanctum')->group(function () {
+    Route::prefix('v1')->group(function () {
 
-    Route::apiResource('users', UserController::class);
-    Route::apiResource('collections', CollectionController::class);
-    Route::apiResource('resources', ResourceController::class);
+        Route::apiResource('users', UserController::class);
+        Route::apiResource('collections', CollectionController::class);
+        Route::apiResource('resources', ResourceController::class);
 
-    // Custom routes
-    Route::get('users/{userId}/collections', [CollectionController::class, 'getByUser']);
-    Route::get('users/{userId}/collections/{parentId}', [CollectionController::class, 'getByUserAndParent']);
+        // Custom routes
+        Route::get('users/{userId}/collections', [CollectionController::class, 'getByUser']);
+        Route::get('users/{userId}/collections/{parentId}', [CollectionController::class, 'getByUserAndParent']);
 
-    Route::get('users/{userId}/resources', [ResourceController::class, 'getUserResources']);
-    Route::get('users/{userId}/collections/{collectionId}/resources', [ResourceController::class, 'getByUserAndCollection']);
+        Route::get('users/{userId}/resources', [ResourceController::class, 'getUserResources']);
+        Route::get('users/{userId}/collections/{collectionId}/resources', [ResourceController::class, 'getByUserAndCollection']);
 
-    // Auth routes 
-    Route::post('logout', [AuthController::class, 'logout']);
-    Route::get('me', [AuthController::class, 'me']);
-    
-})->middleware('auth:sanctum');
+        // Auth routes
+        Route::post('logout', [AuthController::class, 'logout']);
+        Route::get('me', [AuthController::class, 'me']);
+    });
+});
