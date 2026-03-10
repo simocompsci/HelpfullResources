2049e4c57f32d9ee49b718f4b0fa432512b14912
simocompsci <hamids9alo7777@gmail.com>
2026-03-10 23:33:27 +0000

    working on auth

--- backend/bootstrap/app.php
+++ backend/bootstrap/app.php
@@ -3,17 +3,28 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Auth\AuthenticationException;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
         web: __DIR__.'/../routes/web.php',
         commands: __DIR__.'/../routes/console.php',
-        api:__DIR__.'/../routes/api.php',
+        api: __DIR__.'/../routes/api.php',
         health: '/up',
     )
+
     ->withMiddleware(function (Middleware $middleware): void {
         //
     })
+
     ->withExceptions(function (Exceptions $exceptions): void {
-        //
-    })->create();
+
+        $exceptions->render(function (AuthenticationException $e, $request) {
+            return response()->json([
+                'message' => 'Unauthenticated'
+            ], 401);
+        });
+
+    })
+
+    ->create();
