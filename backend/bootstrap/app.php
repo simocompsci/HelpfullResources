2d30c98ffdf9477b58df597e0f4c0ee3e9c3fdcd
simocompsci <hamids9alo7777@gmail.com>
2026-03-11 22:02:43 +0000

    working on auth

--- backend/bootstrap/app.php
+++ backend/bootstrap/app.php
@@ -19,11 +19,7 @@ return Application::configure(basePath: dirname(__DIR__))
 
     ->withExceptions(function (Exceptions $exceptions): void {
 
-        $exceptions->render(function (AuthenticationException $e, $request) {
-            return response()->json([
-                'message' => 'Unauthenticated'
-            ], 401);
-        });
+        //
 
     })
 
