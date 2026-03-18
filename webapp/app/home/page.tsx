11b8a584bfe4e1c12d7d05653b9a46d00b0f6c02
simocompsci <hamids9alo7777@gmail.com>
2026-03-18 12:12:06 +0000

    now starting the work on the home page

--- webapp/app/home/page.tsx
+++ webapp/app/home/page.tsx
@@ -1,3 +1,11 @@
-export default function HomePage(){
-
+export default function HomePage() {
+  return (
+    <>
+      <div className="mx-100 border-x-2 border-black">
+        <h1 className="text-center ">
+          This will be our home page and the first thing the user interacts with
+        </h1>
+      </div>
+    </>
+  );
 }
