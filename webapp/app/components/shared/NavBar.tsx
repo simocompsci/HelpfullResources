38ad2ef561a17511b535e935df837ee272c27c2c
simocompsci <hamids9alo7777@gmail.com>
2026-03-25 12:12:04 +0100

    changed the style of the navbar ini the landing page

--- webapp/app/components/shared/NavBar.tsx
+++ webapp/app/components/shared/NavBar.tsx
@@ -1,6 +1,6 @@
 export default function NavBar() {
   return (
-    <nav className="flex items-center gap-6 px-6 md:px-16 py-6 rounded-full backdrop-blur-xl bg-gray-200 border border-gray-300">
+    <nav className="flex items-center  px-6 md:px-16 py-6 rounded-full backdrop-blur-xl bg-gray-200 border border-gray-300">
       {/* Links */}
       <ul className="flex gap-4 text-sm md:gap-8  md:text-base lg:text-xl font-medium">
         <li className="cursor-pointer">Home</li>
