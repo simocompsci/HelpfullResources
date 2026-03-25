9cf7be656a1b61f638be0681fea1b419fd2fa35d
simocompsci <hamids9alo7777@gmail.com>
2026-03-25 12:03:27 +0100

    changed the style of the navbar ini the landing page

--- webapp/app/(main)/home/page.tsx
+++ webapp/app/(main)/home/page.tsx
@@ -1,7 +1,7 @@
 export default function HomePage() {
   return (
     <>
-      <div className="mx-100 border-x-2 border-black">
+      <div className="bg-gray-200 ">
         <h1 className="text-center ">
           This will be our home page and the first thing the user interacts with
         </h1>
--- webapp/app/(main)/layout.tsx
+++ webapp/app/(main)/layout.tsx
@@ -1,20 +1,19 @@
 import NavBar from "../components/shared/NavBar";
 import type { ReactNode } from "react";
 
-export default function MainLayout({
-  children,
-}: {
-  children: ReactNode;
-}) {
+export default function MainLayout({ children }: { children: ReactNode }) {
   return (
     <>
-      <NavBar />
-
-      <main className="pt-24">
+      <main className="pt-24 pb-24">
         <div className="mx-auto w-full max-w-7xl px-4 sm:px-6 lg:px-8">
           {children}
         </div>
       </main>
+
+      {/* Bottom Center Navbar */}
+      <div className="fixed bottom-25 md:bottom-10 left-1/2 -translate-x-1/2 z-50">
+        <NavBar />
+      </div>
     </>
   );
 }
