65845e4225524fcad35fef856229fd188e12d47d
simocompsci <hamids9alo7777@gmail.com>
2026-03-25 11:48:47 +0100

    changed the style of the navbar ini the landing page

--- webapp/app/(main)/layout.tsx
+++ webapp/app/(main)/layout.tsx
@@ -1,5 +1,11 @@
 import NavBar from "../components/shared/NavBar";
-export default function MainLayout({ children }) {
+import type { ReactNode } from "react";
+
+export default function MainLayout({
+  children,
+}: {
+  children: ReactNode;
+}) {
   return (
     <>
       <NavBar />
