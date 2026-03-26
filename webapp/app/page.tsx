74db3dbd326d6e78feba67ed6b3857a4593a8799
simocompsci <hamids9alo7777@gmail.com>
2026-03-26 12:46:51 +0100

    changed the style of the navbar ini the landing page

---
+++ webapp/app/page.tsx
@@ -0,0 +1,17 @@
+import About from "./components/Landing/About";
+import Features from "./components/Landing/Features";
+import Footer from "./components/Landing/Footer";
+import Hero from "./components/Landing/Hero";
+import Navbar from "./components/Landing/Navbar";
+import Testimonials from "./components/Landing/Testimonials";
+
+export default function Page(){
+    <>
+        <Navbar/>
+        <Hero/>
+        <About/>
+        <Features/>
+        <Testimonials/>
+        <Footer/>
+    </>
+}
