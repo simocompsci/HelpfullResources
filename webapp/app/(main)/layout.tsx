5dec67f7625491f6471a8b11cf54f326f6e7b1f8
simocompsci <hamids9alo7777@gmail.com>
2026-04-09 13:35:09 +0100

    some styling changes

--- webapp/app/(main)/layout.tsx
+++ webapp/app/(main)/layout.tsx
@@ -20,14 +20,14 @@ export default function MainLayout({ children }: { children: React.ReactNode }) 
       </main>
 
       {/* Floating Bottom Navigation */}
-      <div className="fixed bottom-4 sm:bottom-8 left-1/2 transform -translate-x-1/2 w-[95%] sm:w-auto bg-white border border-gray-300 rounded-3xl px-4 sm:px-6 py-3 sm:py-4 flex items-center justify-between md:gap-6 gap-2 z-50 shadow-sm text-center">
+      <div className="fixed bottom-4 sm:bottom-8 left-1/2 transform -translate-x-1/2 w-[95%] sm:w-auto bg-white border border-gray-300 rounded-full px-4 sm:px-6 py-3 sm:py-4 flex items-center justify-between md:gap-6 gap-2 z-50 shadow-sm text-center">
 
-        <Link href="/home" className={`flex flex-col items-center gap-1 active:scale-95 transition-all px-4 sm:px-5 md:px-6 py-2 rounded-xl border border-transparent ${pathname === '/home' ? 'bg-[#f0efff] text-[#242c51]' : 'text-[#515981] hover:text-[#0a79ff]'}`}>
+        <Link href="/home" className={`flex flex-col items-center gap-1 active:scale-100 transition-all px-4 sm:px-5 md:px-6 py-2 rounded-full border border-transparent ${pathname === '/home' ? 'bg-[#f0efff] text-[#242c51]' : 'text-[#515981] hover:text-[#0a79ff]'}`}>
           <Home size={20} className={pathname === '/home' ? "fill-[#242c51]" : ""} />
           <span className="font-mono font-bold text-[9px] sm:text-[10px] tracking-widest uppercase">Home</span>
         </Link>
 
-        <Link href="/collections" className={`flex flex-col items-center gap-1 active:scale-95 transition-all px-4 sm:px-5 md:px-6 py-2 rounded-xl border border-transparent ${pathname === '/collections' ? 'bg-[#f0efff] text-[#242c51]' : 'text-[#515981] hover:text-[#0a79ff]'}`}>
+        <Link href="/collections" className={`flex flex-col items-center gap-1 active:scale-100 transition-all px-4 sm:px-5 md:px-6 py-2 rounded-full border border-transparent ${pathname === '/collections' ? 'bg-[#f0efff] text-[#242c51]' : 'text-[#515981] hover:text-[#0a79ff]'}`}>
           <FolderHeart size={20} className={pathname === '/collections' ? "fill-[#242c51]" : ""} />
           <span className="font-mono font-bold text-[9px] sm:text-[10px] tracking-widest uppercase">Collections</span>
         </Link>
