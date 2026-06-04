185df55300a7951e87accc04a6ea22d4309b02bf
simocompsci <hamids9alo7777@gmail.com>
2026-06-04 21:46:23 +0100

    some ui changes in the landing page

--- webapp/app/page.tsx
+++ webapp/app/page.tsx
@@ -1,23 +1,18 @@
-import About from "./components/Landing/About";
-import Features from "./components/Landing/Features";
-import Footer from "./components/Landing/Footer";
-import Hero from "./components/Landing/Hero";
-import Navbar from "./components/Landing/Navbar";
-import Testimonials from "./components/Landing/Testimonials";
-import FAQ from "./components/Landing/FAQ";
-import ContactUs from "./components/Landing/ContactUs";
+// import About from "./components/Landing/About";
+// import Features from "./components/Landing/Features";
+// import Footer from "./components/Landing/Footer";
+// import Hero from "./components/Landing/Hero";
+// import Navbar from "./components/Landing/Navbar";
+// import Testimonials from "./components/Landing/Testimonials";
+// import FAQ from "./components/Landing/FAQ";
+// import ContactUs from "./components/Landing/ContactUs";
 
 export default function Page() {
     return (
         <>
-            <Navbar />
-            <Hero />
-            <About />
-            <Features />
-            <Testimonials />
-            <FAQ />
-            <ContactUs />
-            <Footer />
+            <nav>
+                This is the landing Page
+            </nav>
         </>
     );
 }
