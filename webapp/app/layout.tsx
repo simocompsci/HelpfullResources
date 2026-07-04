3729ef003af49aa2d921aa684f6096ad5fe39e1e
simocompsci <hamids9alo7777@gmail.com>
2026-07-04 15:13:25 +0100

    changing the fonts

--- webapp/app/layout.tsx
+++ webapp/app/layout.tsx
@@ -1,15 +1,15 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Inter, Playfair_Display } from "next/font/google";
 import "./globals.css";
 import { Toaster } from "sonner";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
+const inter = Inter({
+  variable: "--font-inter",
   subsets: ["latin"],
 });
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
+const playfairDisplay = Playfair_Display({
+  variable: "--font-playfair-display",
   subsets: ["latin"],
 });
 
@@ -24,8 +24,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body>
+    <html lang="en" className={`${inter.variable} ${playfairDisplay.variable}`}>
+      <body className="font-sans">
         {children}
         <Toaster position="bottom-right" richColors />
       </body>
