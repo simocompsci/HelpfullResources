cbcd9f0e06418f45b961a8a78793dea85b2d6bbf
simocompsci <hamids9alo7777@gmail.com>
2026-07-04 15:08:09 +0100

    changing the fonts

--- webapp/app/components/Landing/Navbar.tsx
+++ webapp/app/components/Landing/Navbar.tsx
@@ -1,13 +1,18 @@
 "use client"
+import { Inter } from "next/font/google"
+
+const inter = Inter({
+  subsets: ["latin"],
+});
 
 export default function NavBar(){
   return (
-    <nav>
-      <ul>
-        <li>About Us</li>
-        <li>Features</li>
-        <li>Platforms</li>
-        <li>Contact Us</li>
+    <nav className="flex justify-center font-medium text-[2.5rem]/[2.75rem] tracking-[-0.8px] text-text-main lg:text-3xl lg:tracking-[-1px]">
+      <ul className={`flex justify-between gap-4 font-inter ${inter.className}`}>
+        <li>Products</li>
+        <li>Resources</li>
+        <li>Solutions</li>
+        <li>Pricing</li>
       </ul>
     </nav>
   )
--- webapp/app/page.tsx
+++ webapp/app/page.tsx
@@ -2,17 +2,27 @@
 // import Features from "./components/Landing/Features";
 // import Footer from "./components/Landing/Footer";
 // import Hero from "./components/Landing/Hero";
+import NavBar from "./components/Landing/Navbar";
 // import Navbar from "./components/Landing/Navbar";
 // import Testimonials from "./components/Landing/Testimonials";
 // import FAQ from "./components/Landing/FAQ";
 // import ContactUs from "./components/Landing/ContactUs";
 
+import { Inter } from "next/font/google"
+
+const inter = Inter({
+  subsets: ["latin"],
+});
+
 export default function Page() {
     return (
         <>
+        <NavBar/>
         <div className="w-4xl bg-gray-200 flex justify-center mx-auto mt-50 border border-black">
-            <div className="flex text-9xl text-center font-bold font-serif p-10">
-                This is the landing Page
+            <div className={`flex font-medium text-center text-lg/6 text-text-sub p-10 ${inter.className}`}>
+                Self-updating documentation for startups, enterprises, and agents.
+
+
             </div>
         </div>
             
