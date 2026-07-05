eea4461746da68c81a3e09901bb3993333f194cf
simocompsci <hamids9alo7777@gmail.com>
2026-07-05 15:54:35 +0100

    working on the landing page

--- webapp/app/components/Landing/Hero.tsx
+++ webapp/app/components/Landing/Hero.tsx
@@ -1,8 +1,11 @@
 export default function Hero() {
   return (
     <section className="flex min-h-[calc(100dvh-4rem)]">
-      <div className="mx-auto flex w-full max-w-6xl items-center justify-center bg-gray-300">
-        <p>This is the hero</p>
+      <div className="mx-auto flex w-full max-w-6xl items-center justify-center">
+        <div>
+          
+        </div>
+        <div></div>
       </div>
     </section>
   );
