0ad0dadaed1fcb2405fb695b56997166af3d78ce
simocompsci <hamids9alo7777@gmail.com>
2026-07-05 16:07:34 +0100

    working on the landing page

--- webapp/app/components/Landing/Hero.tsx
+++ webapp/app/components/Landing/Hero.tsx
@@ -3,9 +3,11 @@ export default function Hero() {
     <section className="flex min-h-[calc(100dvh-4rem)]">
       <div className="mx-auto flex w-full max-w-6xl items-center justify-center">
         <div>
-          
+          {/* the images of our application */}
         </div>
-        <div></div>
+        <div>
+          {/* The CTA section */}
+        </div>
       </div>
     </section>
   );
