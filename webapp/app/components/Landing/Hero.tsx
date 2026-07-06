050dc8d5ab1fce025db6b103750c8c4b7819e4fd
simocompsci <hamids9alo7777@gmail.com>
2026-07-06 12:05:02 +0100

    working on the landing page

--- webapp/app/components/Landing/Hero.tsx
+++ webapp/app/components/Landing/Hero.tsx
@@ -1,17 +1,36 @@
 import Image from "next/image";
-import webHero from "@/public/images/webHero.png";
 import mobileHero from "@/public/images/mobileHero.png";
 
 export default function Hero() {
   return (
     <section className="flex min-h-[calc(100dvh-4rem)]">
-      <div className="mx-auto flex w-full max-w-6xl items-center justify-center">
-        <div>
-          {/* the images of our application */}
-          <Image  src={webHero} alt="webHero" />
-          <Image src={mobileHero} alt="mobileHero" />
+      <div className="mx-auto grid w-full max-w-6xl grid-cols-1 items-center gap-8 lg:grid-cols-2">
+        {/* Images section */}
+        <div className="flex justify-center">
+          <Image
+            src={mobileHero}
+            alt="mobileHero"
+            height={1000}
+            className="h-auto w-auto"
+          />
         </div>
-        <div>{/* The CTA section */}</div>
+
+        {/* CTA section */}
+        <div className="flex flex-col justify-center space-y-4">
+          <h1 className="font-serif text-[2.5rem]/[2.75rem] tracking-[-0.8px] lg:text-[3.125rem]/[3.25rem] lg:tracking-[-0.8px] font-medium">
+            Big Catchy Phrase
+          </h1>
+
+          <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit.</p>
+          <div className="flex gap-2 font-medium">
+            <button className="bg-black text-white py-3 px-3 rounded-sm">
+              Get it on the App Store
+            </button>
+            <button className="bg-black text-white py-3 px-3 rounded-sm">
+              Get it on Google Play
+            </button>
+          </div>
+        </div>
       </div>
     </section>
   );
