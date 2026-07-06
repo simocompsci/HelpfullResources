c9ee7f94a6b1f65adf6bd3176bca9298fe23db79
simocompsci <hamids9alo7777@gmail.com>
2026-07-06 11:33:04 +0100

    working on the landing page

--- webapp/app/components/Landing/Hero.tsx
+++ webapp/app/components/Landing/Hero.tsx
@@ -1,13 +1,17 @@
+import Image from "next/image";
+import webHero from "@/public/images/webHero.png";
+import mobileHero from "@/public/images/mobileHero.png";
+
 export default function Hero() {
   return (
     <section className="flex min-h-[calc(100dvh-4rem)]">
       <div className="mx-auto flex w-full max-w-6xl items-center justify-center">
         <div>
           {/* the images of our application */}
+          <Image  src={webHero} alt="webHero" />
+          <Image src={mobileHero} alt="mobileHero" />
         </div>
-        <div>
-          {/* The CTA section */}
-        </div>
+        <div>{/* The CTA section */}</div>
       </div>
     </section>
   );
