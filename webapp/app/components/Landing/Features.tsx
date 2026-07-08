0684822a02c5cf0937ec54da7088c6ed92226791
simocompsci <hamids9alo7777@gmail.com>
2026-07-08 16:52:26 +0100

    working on the landing page

--- webapp/app/components/Landing/Features.tsx
+++ webapp/app/components/Landing/Features.tsx
@@ -3,13 +3,17 @@ export default function Features() {
     <section className="mt-16">
       <div className="mx-auto max-w-6xl px-5">
         <div className="bg-black rounded-[2px]">
-          <div>
-            <h1 className="px-5 text-white">the features section</h1>
-            <h1 className="px-5 text-white">the features section</h1>
-            <h1 className="px-5 text-white">the features section</h1>
+          <div  className="flex justify-between">
+            <div>
+              <h1 className="px-5 text-white">the features section</h1>
+              <h1 className="px-5 text-white">the features section</h1>
+              <h1 className="px-5 text-white">the features section</h1>
+            </div>
+            <div>
+              <button className="text-white px-5">Get Started</button>
+            </div>
           </div>
           <div></div>
-          
         </div>
       </div>
     </section>
