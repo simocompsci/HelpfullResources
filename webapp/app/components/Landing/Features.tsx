50c8abc0e10a1f3f92dfc08a3fc95598816033fd
simocompsci <hamids9alo7777@gmail.com>
2026-07-08 17:57:04 +0100

    working on the landing page

--- webapp/app/components/Landing/Features.tsx
+++ webapp/app/components/Landing/Features.tsx
@@ -2,16 +2,26 @@ export default function Features() {
   return (
     <section className="mt-16">
       <div className="mx-auto max-w-6xl px-5">
-        <div className="bg-black rounded-[2px] py-5">
-          <div  className="flex justify-between">
+        <div className="bg-white rounded-[2px] py-5 px-5">
+          <div className="flex flex-col gap-5 md:flex md:flex-row  justify-between md:items-end">
             <div className="text-[1.75rem]/8 font-medium tracking-[-0.72px] text-foreground-primary lg:text-[2.25rem]/[2.5rem]">
-              <p className="px-5 text-white max-w-xl text-balance">The features section The features section The features section section</p>
+              <p className="text-black max-w-xl text-balance">
+                The features section The features section The features section
+                section
+              </p>
             </div>
             <div>
-              <button className="text-white px-5">Get Started</button>
+              <button className="text-white rounded-[2px] bg-black py-3 px-5">
+                Get Started
+              </button>
             </div>
           </div>
-          <div></div>
+          <div className="grid grid-cols-1 md:grid-cols-2 gap-5 mt-5">
+            <div className="aspect-square bg-gray-200 rounded-[2px]"></div>
+            <div className="aspect-square bg-gray-200 rounded-[2px]"></div>
+            <div className="aspect-square bg-gray-200 rounded-[2px]"></div>
+            <div className="aspect-square bg-gray-200 rounded-[2px]"></div>
+          </div>
         </div>
       </div>
     </section>
