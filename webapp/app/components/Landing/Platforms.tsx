43173dc0e7a422b4327772853670b5f2f755ffa7
simocompsci <hamids9alo7777@gmail.com>
2026-07-19 21:11:25 +0100

    adding the platfroms section

--- webapp/app/components/Landing/Platforms.tsx
+++ webapp/app/components/Landing/Platforms.tsx
@@ -3,8 +3,16 @@ export default function Platforms() {
     <section className="mt-16 border-y border-gray-300">
       <div className="mx-auto max-w-6xl md:px-5">
         <div className="bg-white rounded-[2px] py-5 px-5 border-x border-gray-300">
-            <h1 className="text-4xl font-medium text-black">
-                Easy To Access From All Platforms</h1>
+          <div className="flex flex-col gap-5  justify-between">
+            <h1 className="text-4xl font-medium text-black max-w-xl text-balance">
+              Easy To Access{" "}
+              <span className="text-gray-600"> From All Platforms</span>{" "}
+            </h1>
+            <div className="bg-gray-500">
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor architecto aspernatur repellendus inventore. Temporibus ratione accusantium adipisci quae, fuga labore quisquam quo voluptate assumenda recusandae quos tempore ipsam, numquam tempora?</p>
+
+            </div>
+          </div>
         </div>
       </div>
     </section>
