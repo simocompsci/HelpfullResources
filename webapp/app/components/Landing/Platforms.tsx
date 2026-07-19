bfd2de02b25d13a5f300c2586d7e50cb447e3a61
simocompsci <hamids9alo7777@gmail.com>
2026-07-19 21:06:29 +0100

    adding the platfroms section

--- webapp/app/components/Landing/Platforms.tsx
+++ webapp/app/components/Landing/Platforms.tsx
@@ -1,7 +1,12 @@
-export default function Platforms(){
-    return (
-        <div>
-            The Platforms section
+export default function Platforms() {
+  return (
+    <section className="mt-16 border-y border-gray-300">
+      <div className="mx-auto max-w-6xl md:px-5">
+        <div className="bg-white rounded-[2px] py-5 px-5 border-x border-gray-300">
+            <h1 className="text-4xl font-medium text-black">
+                Easy To Access From All Platforms</h1>
         </div>
-    )
+      </div>
+    </section>
+  );
 }
--- webapp/app/page.tsx
+++ webapp/app/page.tsx
@@ -2,6 +2,7 @@ import About from "./components/Landing/About";
 import Features from "./components/Landing/Features";
 import Hero from "./components/Landing/Hero";
 import NavBar from "./components/Landing/Navbar";
+import Platforms from "./components/Landing/Platforms";
 
 export default function Page() {
   return (
@@ -11,6 +12,7 @@ export default function Page() {
         <Hero/>
         <About/>
         <Features/>
+        <Platforms/>
       </main>
     </>
   );
