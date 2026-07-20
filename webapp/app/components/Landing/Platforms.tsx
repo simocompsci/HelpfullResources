b4e1987213e5868cd61bc702344075b9af491cca
simocompsci <hamids9alo7777@gmail.com>
2026-07-20 21:31:24 +0100

    adding my profile image

--- webapp/app/components/Landing/Platforms.tsx
+++ webapp/app/components/Landing/Platforms.tsx
@@ -1,34 +1,34 @@
-export default function Platforms() {
-  return (
-    <section className="mt-16 border-y border-gray-300">
-      <div className="mx-auto max-w-6xl md:px-5">
-        <div className="bg-white  border-x border-gray-300">
-          <div className="flex flex-col gap-5  justify-between">
-            <div className="border-b py-5 px-5 border-gray-300">
-              <h1 className="text-4xl font-medium text-black max-w-xl text-balance">
-              Easy To Access{" "}
-              <span className="text-gray-600"> From All Platforms</span>{" "}
-            </h1>
+  export default function Platforms() {
+    return (
+      <section className="mt-16 border-y border-gray-300">
+        <div className="mx-auto max-w-6xl px-5">
+          <div className="bg-white border-gray-300">
+            <div className="flex flex-col gap-5  justify-between">
+              <div className="border-b border-x py-5 px-5 border-gray-300">
+                <h1 className="text-black max-w-lg md:max-w-lg text-balance text-[1.75rem]/8 font-medium tracking-[-0.72px] lg:text-[2.25rem]/[2.5rem]">
+                Easy To Access{" "}
+                <span className="text-gray-600"> From All Platforms</span>{" "}
+              </h1>
 
-            </div>
-            
-            <div className="grid grid-cols-2 py-5 px-5 gap-5 md:grid-cols-4">
-              <div className="h-60 rounded-[3px] bg-gray-200"></div>
-              <div className="h-60 rounded-[3px] bg-gray-200"></div>
-              <div className="h-60 rounded-[3px] bg-gray-200"></div>
-              <div className="h-60 rounded-[3px] bg-gray-200"></div>
-              <div className="h-60 rounded-[3px] bg-gray-200"></div>
-              <div className="h-60 rounded-[3px] bg-gray-200"></div>
-              <div className="h-60 rounded-[3px] bg-gray-200"></div>
-              <div className="h-60 rounded-[3px] bg-gray-200"></div>
-              <div className="h-60 rounded-[3px] bg-gray-200"></div>
-              <div className="h-60 rounded-[3px] bg-gray-200"></div>
-              <div className="h-60 rounded-[3px] bg-gray-200"></div>
-              <div className="h-60 rounded-[3px] bg-gray-200"></div>
+              </div>
+              
+              <div className="grid grid-cols-2  py-5 md:px-5 gap-5 md:grid-cols-4">
+                <div className="h-60 rounded-[3px] bg-gray-200"></div>
+                <div className="h-60 rounded-[3px] bg-gray-200"></div>
+                <div className="h-60 rounded-[3px] bg-gray-200"></div>
+                <div className="h-60 rounded-[3px] bg-gray-200"></div>
+                <div className="h-60 rounded-[3px] bg-gray-200"></div>
+                <div className="h-60 rounded-[3px] bg-gray-200"></div>
+                <div className="h-60 rounded-[3px] bg-gray-200"></div>
+                <div className="h-60 rounded-[3px] bg-gray-200"></div>
+                <div className="h-60 rounded-[3px] bg-gray-200"></div>
+                <div className="h-60 rounded-[3px] bg-gray-200"></div>
+                <div className="h-60 rounded-[3px] bg-gray-200"></div>
+                <div className="h-60 rounded-[3px] bg-gray-200"></div>
+              </div>
             </div>
           </div>
         </div>
-      </div>
-    </section>
-  );
-}
+      </section>
+    );
+  }
