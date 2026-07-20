643608193b54c1a41a1c09f73b100d4d09407762
simocompsci <hamids9alo7777@gmail.com>
2026-07-20 21:17:44 +0100

    adding my profile image

--- webapp/app/components/Landing/Platforms.tsx
+++ webapp/app/components/Landing/Platforms.tsx
@@ -2,15 +2,29 @@ export default function Platforms() {
   return (
     <section className="mt-16 border-y border-gray-300">
       <div className="mx-auto max-w-6xl md:px-5">
-        <div className="bg-white rounded-[2px] py-5 px-5 border-x border-gray-300">
+        <div className="bg-white  border-x border-gray-300">
           <div className="flex flex-col gap-5  justify-between">
-            <h1 className="text-4xl font-medium text-black max-w-xl text-balance">
+            <div className="border-b py-5 px-5 border-gray-300">
+              <h1 className="text-4xl font-medium text-black max-w-xl text-balance">
               Easy To Access{" "}
               <span className="text-gray-600"> From All Platforms</span>{" "}
             </h1>
-            <div className="bg-gray-500">
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor architecto aspernatur repellendus inventore. Temporibus ratione accusantium adipisci quae, fuga labore quisquam quo voluptate assumenda recusandae quos tempore ipsam, numquam tempora?</p>
 
+            </div>
+            
+            <div className="grid grid-cols-2 py-5 px-5 gap-5 md:grid-cols-4">
+              <div className="h-60 rounded-[3px] bg-gray-200"></div>
+              <div className="h-60 rounded-[3px] bg-gray-200"></div>
+              <div className="h-60 rounded-[3px] bg-gray-200"></div>
+              <div className="h-60 rounded-[3px] bg-gray-200"></div>
+              <div className="h-60 rounded-[3px] bg-gray-200"></div>
+              <div className="h-60 rounded-[3px] bg-gray-200"></div>
+              <div className="h-60 rounded-[3px] bg-gray-200"></div>
+              <div className="h-60 rounded-[3px] bg-gray-200"></div>
+              <div className="h-60 rounded-[3px] bg-gray-200"></div>
+              <div className="h-60 rounded-[3px] bg-gray-200"></div>
+              <div className="h-60 rounded-[3px] bg-gray-200"></div>
+              <div className="h-60 rounded-[3px] bg-gray-200"></div>
             </div>
           </div>
         </div>
